41d44c80170680a4f2a3e9050f0ba5f7206d604d
Vasiliy <ieatmybrain@gmail.com>
2019-05-22 20:01:44 +0300

    replace victory to react-chartjs-2

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { VictoryChart, VictoryZoomContainer, VictoryLine } from "victory";
+import { Line } from 'react-chartjs-2';
 
 class Chart extends React.Component {
     constructor() {
       super();
       this.state = {
-        
+        show: false,
         open: false,
         connected: false,
-        quotes: []
+        quotes: [],
+        newDate: []
       };
       this.socket = new WebSocket('wss://websocket.uptrader.io/');
       this.socket.onopen = () => {
@@ -26,9 +27,17 @@ class Chart extends React.Component {
       this.socket.onopen = () => this.socket.send("BTCUSD");
       this.socket.onmessage = ({data}) => {
         let quotesData = JSON.parse(data)
+
+        let date = new Date(quotesData.time * 1000)
+        let hours = date.getHours();
+        let minutes = "0" + date.getMinutes();
+        let seconds = "0" + date.getSeconds();
+        let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
         this.setState(
-          {quotes: quotesData}
+          {quotes: quotesData, show: true, newDate: formattedTime}
         )
+
       };
     }
 
@@ -37,22 +46,54 @@ class Chart extends React.Component {
     }
     
     render() {
+      const data = {
+        labels: [],
+        datasets: [
+          {
+            label: '',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            borderWidth: 1,
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: []
+          }
+        ]
+      };
+      const options = {
+        responsive: true,
+        title: {
+          display: false,
+        },
+        legend: {
+          display: false
+        },
+      }
+      data.datasets[0].data.push(this.state.quotes.bid)
+      data.labels.push(`Time: ${this.state.newDate}`);
+      // Line.update();
       return (
         <div>
           <div>{this.state.quotes.bid}</div>
-          <VictoryChart width={600} height={470} >
-              <VictoryLine
-                style={{
-                  data: { stroke: "tomato" }
-                }}
-                data={[
-                  { x: this.state.quotes.bid, y: this.state.quotes.time }
-                ]}
-              />
-  
-            </VictoryChart>
-            
-        </div>
+          <Line
+            data={data}
+            options={options}
+          >
+          </Line>
+        </div> 
       );
     }
   }
