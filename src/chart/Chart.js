8f4c08e6f4c287a64394b4a3aa38fec3147470d7
Vasiliy <ieatmybrain@gmail.com>
2019-05-22 20:56:03 +0300

    nc

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -4,13 +4,54 @@ import { Line } from 'react-chartjs-2';
 class Chart extends React.Component {
     constructor() {
       super();
+
       this.state = {
         show: false,
         open: false,
         connected: false,
         quotes: [],
-        newDate: []
+        newDate: [],
+        bid: '',
+
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: '',
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              borderWidth: 1,
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: []
+            }
+          ]
+        }
       };
+
+      this.options = {
+        responsive: true,
+        title: {
+          display: false,
+        },
+        legend: {
+          display: false
+        },
+      }
+
       this.socket = new WebSocket('wss://websocket.uptrader.io/');
       this.socket.onopen = () => {
         this.setState({connected:true})
@@ -25,6 +66,7 @@ class Chart extends React.Component {
     }
     componentDidMount(){
       this.socket.onopen = () => this.socket.send("BTCUSD");
+
       this.socket.onmessage = ({data}) => {
         let quotesData = JSON.parse(data)
 
@@ -34,63 +76,33 @@ class Chart extends React.Component {
         let seconds = "0" + date.getSeconds();
         let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
+        data.datasets[0].data.push(quotesData.bid)
+        data.labels.push(`Time: ${formattedTime}`)
+
+        const updatedData = 
+          this.setState(prevState => ({ data: {...prevState.data, datasets:[...prevState.data.datasets[0].data, quotesData.bid] } }))
+        
+
         this.setState(
-          {quotes: quotesData, show: true, newDate: formattedTime}
+          {
+            quotes: quotesData, 
+            show: true, 
+            newDate: formattedTime, 
+            data: data
+          }
         )
-
       };
-    }
-
-    handleZoom(domain) {
-      this.setState({ zoomDomain: domain });
     }
     
     render() {
-      const data = {
-        labels: [],
-        datasets: [
-          {
-            label: '',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            borderWidth: 1,
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: []
-          }
-        ]
-      };
-      const options = {
-        responsive: true,
-        title: {
-          display: false,
-        },
-        legend: {
-          display: false
-        },
-      }
-      data.datasets[0].data.push(this.state.quotes.bid)
-      data.labels.push(`Time: ${this.state.newDate}`);
+
       // Line.update();
       return (
         <div>
           <div>{this.state.quotes.bid}</div>
           <Line
-            data={data}
-            options={options}
+            data={this.state.data}
+            options={this.options}
           >
           </Line>
         </div> 
